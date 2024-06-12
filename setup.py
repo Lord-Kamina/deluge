38e500e95886d8a7f7c9a50e2b5eca03818d9e46
Gregorio Litenstein <g.litenstein@gmail.com>
2024-06-12 12:16:22 -0400

    setup.py lint

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ import glob
 import os
 import platform
 import sys
+from shutil import rmtree, which
 
 from build import ProjectBuilder
 from setuptools import Command, setup
 from setuptools.command.build import build as _build
 from setuptools.command.test import test as _test
-from shutil import rmtree, which
 
 sys.path.append(os.path.dirname(__file__))
 import msgfmt
