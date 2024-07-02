e1a3815725c9da2bb6b1f892b24eeafadf5103a1
Gregorio Litenstein <g.litenstein@gmail.com>
2024-07-02 11:51:44 -0400

    Remove obsolete plugin metadata test

--- deluge/tests/test_plugin_metadata.py
+++ deluge/tests/test_plugin_metadata.py
@@ -22,22 +22,3 @@ class TestPluginManagerBase:
         for key, value in pm.get_plugin_info('random').items():
             result = 'not available' if key in ('Name', 'Version') else ''
             assert value == result
-
-    def test_parse_pkg_info_metadata_2_1(self):
-        pkg_info = """Metadata-Version: 2.1
-Name: AutoAdd
-Version: 1.8
-Summary: Monitors folders for .torrent files.
-Home-page: http://dev.deluge-torrent.org/wiki/Plugins/AutoAdd
-Author: Chase Sterling, Pedro Algarvio
-Author-email: chase.sterling@gmail.com, pedro@algarvio.me
-License: GPLv3
-Platform: UNKNOWN
-
-Monitors folders for .torrent files.
-        """
-        plugin_info = PluginManagerBase.parse_pkg_info(pkg_info)
-        for value in plugin_info.values():
-            assert value != ''
-        result = 'Monitors folders for .torrent files.'
-        assert plugin_info['Description'] == result
